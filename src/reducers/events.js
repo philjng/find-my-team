c4322cf3340159066ac41f40af949b843e81f3d1
u8h2b <u8h2b@ugrad.cs.ubc.ca>
2021-06-25 19:49:23 -0700

    changed equalities from Stringify to lodash

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,4 +1,5 @@
 import {combineReducers} from 'redux';
+const _ = require('lodash');
 const events_test_data = [{name: "Soccer game", location: "Empire Field, Vancouver", date: new Date("2021-07-02"), genre: ['sports', 'outdoor', 'soccer'], creator: {name: "John Doe"}, participants: [{name: "John Doe"}, {name: "Susan Thompson"}, {name: "Pablo Johnson"}, {name: "Alex Tim"}], description: "A soccer game for anyone to join", comments: [{user: {name: "Alex Tim"}, text: "Sounds like fun. I'll be there"}, {user: {name: "Harriet Norman"}, text: "Can't make it this time, I'll be there for the next one"}]}, 
                           {name: "Basketball game", location: "Kits Beach, Vancouver", date: new Date("2021-07-05"), genre: ['sports', 'indoor', 'basketball'], creator: {name: "Jane Doo"}, participants: [{name: "Susan Thompson"}, {name: "Gordon Lighthouse"}, {name: "Heather Baller"}], description: "1 v 1 pickup basketball at the Kits beach courts. Playing king's court and aimed at intermediate level players. $5 to enter and winning team keeps the money", comments: [{user:{name: "Heather Baller"}, text: "I'm init to winit"}]}, 
                           {name: "MTG Tournament", location: "Vancouver Convention Centre", date: new Date("2020-01-04"), genre: ['games', 'card games', 'tournaments', 'magic'], creator: {name: "Joan Smith"}, participants: [{name: "Daniel Boyce"}, {name: "Carol Wisnewski"}, {name: "Samuel Zubrus"}, {name: "Gary Lee"}, {name: "Andrew Gergich"}, {name: "Jennifer Quincey"}, {name: "Joel Quincey"}, {name: "Joel Harland"}, {name: "Carla Gregg"}], description: "Magic The Gathering tournament. Winner takes home $25,000 grand prize", comments: []}];
@@ -7,21 +8,8 @@ const events_test_data = [{name: "Soccer game", location: "Empire Field, Vancouv
 
 
 const addParticipant = (user, event, events) => {
-        // let j = -1;
-        // for (let i = 0; i < events.length; i++) {
-        //     if (JSON.stringify(events[i]) === JSON.stringify(event)) {
-        //         j = i;
-        //     }
-        // }
-        let eventIndex = events.findIndex((element) => {return JSON.stringify(element) === JSON.stringify(event)});
-        let containsUser = event.participants.findIndex((element) => {return JSON.stringify(element) === JSON.stringify(user)});
-        // for (let i = 0; i < event.participants.length; i++) {
-        //     if (JSON.stringify(event.participants[i]) === JSON.stringify(user)) {
-        //         containsUser = true;
-        //     }
-        // }
-
-        if (containsUser === -1) {
+        let eventIndex = events.findIndex((element) => {return _.isEqual(element,event)});
+        if (!JSON.stringify(event.participants).includes(JSON.stringify(user))) {
             event.participants = event.participants.concat(user);
         }
         if (eventIndex+1) {
@@ -32,12 +20,7 @@ const addParticipant = (user, event, events) => {
 }
 
 const addComment = (user, text, event, events) => {
-    // let j = -1;
-    //     for (let i = 0; i < events.length; i++) {
-    //         if (JSON.stringify(events[i]) === JSON.stringify(event)) {
-    //             j = i;
-    //         }
-    let eventIndex = events.findIndex((element) => {return JSON.stringify(element) === JSON.stringify(event)});
+    let eventIndex = events.findIndex((element) => {return _.isEqual(element,event)});
         event.comments = event.comments.concat({user: {name: user}, text: text});
             if (eventIndex+1) {
                 events[eventIndex] = event;
@@ -80,14 +63,7 @@ const viewEventDetailReducer = (viewableEvent = null, action) => {
             return action.value;
         case 'PARTICIPANT_JOIN':
             let event = {...action.event};
-            // let containsUser = false;
-            // for (let i = 0; i < event.participants.length; i++) {
-            //     if (JSON.stringify(event.participants[i]) === JSON.stringify(action.user)) {
-            //         containsUser = true;
-            //     }
-            // }
-            let containsUser = event.participants.findIndex((element) => {return JSON.stringify(element) === JSON.stringify(action.user)});
-            if (containsUser === -1) {
+            if (!JSON.stringify(event.participants).includes(JSON.stringify(action.user))) {
                 event.participants = event.participants.concat(action.user);
             }
             return event;
