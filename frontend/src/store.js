9648bacf6f87666967ff681a1c0942da1ccf5ee2
Philip Ng <philipjng21@gmail.com>
2021-07-16 00:10:36 -0700

    devtools

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,5 @@
 import {applyMiddleware, createStore} from "redux";
-import { rootReducer } from "./reducers/reducer";
+import rootReducer from "./reducers/rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension"
 import thunk from "redux-thunk";
 
