861d376f40b3d9f30385e76e6fa5fdf33336f2d3
Sidney <b2p1b@ugrad.cs.ubc.ca>
2021-07-30 03:02:25 -0700

    fix bug

--- frontend/src/api/genericApi.js
+++ frontend/src/api/genericApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let baseURL = "";
 if (process.env.NODE_ENV === "development") {
-  baseURL = "/api";
+  baseURL = "http://localhost:3001";
 } else {
   baseURL = "https://find-my-team.herokuapp.com";
 }
