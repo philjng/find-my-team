5c515b6ddd51e1cc9060b8b48c5115b97bbb1fbf
tsuiswz <tsuiswz@gmail.com>
2021-07-29 20:10:28 -0700

    Remove console log

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -5,7 +5,6 @@ import { useAuth } from "./context/AuthContext";
 function ProtectedRoute({ component: Component, ...rest }) {
   const { currentUser, loading } = useAuth();
 
-  console.log(loading);
   if (loading) {
     return <LoadingPage />;
   }
