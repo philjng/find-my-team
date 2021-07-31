34e0b9198dd860b45defc02412dc4e5c665a78f2
u8h2b <u8h2b@ugrad.cs.ubc.ca>
2021-07-30 18:32:38 -0700

    commit for pr

--- frontend/src/components/Profile/UserListing.js
+++ frontend/src/components/Profile/UserListing.js
@@ -1,13 +1,21 @@
-import { Container, Typography, Button } from "@material-ui/core";
+import { Card, Typography, Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import {styled} from "@material-ui/styles";
+const UserContainer = styled(Card)({
+  borderRadius: "0.25rem",
+  width: "80%",
+  margin: "auto",
+  backgroundColor: "#d6f5ef"
+
+})
 function UserListing(props) {
   return (
-    <Container>
+    <UserContainer>
       <Typography>{props.user.displayName}</Typography>
       <Button>
         <Link to={`/profile/${props.user._id}`}>Profile</Link>
       </Button>
-    </Container>
+    </UserContainer>
   );
 }
 export default UserListing;
