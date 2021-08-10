bf48cff3458a10a660f28bd90a91758c5f97dc64
Philip Ng <philipjng21@gmail.com>
2021-08-10 15:42:24 -0700

    fix participants, made clickable

--- frontend/src/components/Events/EventParticipants.js
+++ frontend/src/components/Events/EventParticipants.js
@@ -1,48 +1,63 @@
-import { Container, Typography, List, ListItem, Box } from "@material-ui/core";
-import { styled } from "@material-ui/styles";
-import { connect } from "react-redux";
-import { getEventParticipants } from "../../actions/events";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import {Typography, List, ListItem, Box} from "@material-ui/core";
+import {styled} from "@material-ui/styles";
+import {connect} from "react-redux";
+import {getEventParticipants} from "../../actions/events";
+import {useEffect} from "react";
+import {useHistory, useParams} from "react-router-dom";
+import CloudinaryAvatar from "../shared-components/CloudinaryAvatar";
 
 const _ = require("lodash");
 
-const ListItem1 = styled(ListItem)({
+const Participant = styled(ListItem)({
+  "&:hover": {
+    cursor: "pointer",
+    backgroundColor: "#f7fdfc"
+  },
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
   border: "1px solid grey",
-  margin: "0.25rem",
-  backgroundColor: "#f7fdfc",
-  width: "25%",
+  marginRight: "1rem",
+  backgroundColor: "white",
+  width: "100px",
+  height: "100px",
 });
 
 const List1 = styled(List)({
   display: "flex",
   flexWrap: "wrap",
-  justifyContent: "center",
   margin: "0.25rem",
 });
 
 function EventParticipants(props) {
-  const { getEventParticipants, participants } = props;
-  const { id } = useParams();
+  const {getEventParticipants, participants} = props;
+  const {id} = useParams();
+  const history = useHistory();
 
   useEffect(() => {
     getEventParticipants(id);
   }, [id, getEventParticipants]);
 
+  const handleClick = (participant) => {
+    history.push(`/profile/${participant._id}`)
+  }
+
   return (
     <Box>
-      <Typography variant="h5">{"Participants ("+ participants.length + ")"}</Typography>
+      <Typography variant="h5">{"Participants (" + participants.length + ")"}</Typography>
       <List1>
         {_.isEmpty(participants) ? (
           // <CircularProgress color="primary" size={100}/> // once we have LOADING state set up
           <></>
         ) : (
           participants.map((participant) => (
-            <ListItem1 key={participant._id}>
-              <Container>
-                <Typography>{participant.displayName}</Typography>
-              </Container>
-            </ListItem1>
+            <Participant key={participant._id} onClick={() => {handleClick(participant)}}>
+              <CloudinaryAvatar
+                publicId={participant.image}
+                size={40}
+              />
+              <Typography>{participant.displayName}</Typography>
+            </Participant>
           ))
         )}
       </List1>
